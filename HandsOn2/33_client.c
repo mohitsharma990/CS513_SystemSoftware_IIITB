96445c916241e634f9d65783e6e4607be5fbf79f
mohitsharma990 <iitgmohitsharma@gmail.com>
2024-09-21 02:36:14 +0530

    all done

---
+++ HandsOn2/33_client.c
@@ -0,0 +1,70 @@
+/*
+============================================================================
+Name : 33_client.c
+Author : Mohit Sharma
+Description : Write a program to communicate between two machines using socket.
+Date: 21 Sept 2024
+============================================================================
+*/
+
+/*
+    1. socket() -> create an endpoint for the communication
+        sockfetfd = socket(AF_INET, SOCK_STREAM, 0);
+    2. connect() -> initiate a connection to a socket
+        connectionStatus = connect(socketfd, &socketaddr, sizeof(socketaddr));
+    3. Start communicating -> `write` to and `read` from socketfd
+*/
+
+#include <sys/types.h>  
+#include <sys/socket.h> 
+#include <netinet/ip.h> 
+#include <stdio.h>      
+#include <unistd.h>     
+void main()
+{
+    int socktd=socket(AF_INET,SOCK_STREAM,0);
+    if(socktd==-1){
+        perror("Erroe when creating socket");
+    }
+    printf("Socket created\n");
+
+    // assigning server info
+    struct sockaddr_in address;
+    address.sin_addr.s_addr=htonl(INADDR_ANY);//host to network short
+    address.sin_family=AF_INET;
+    address.sin_port=htons(8080);
+
+    //make connection to the server
+    int connectionS = connect(socktd, (struct sockaddr *)&address, sizeof(address));
+
+    if(connectionS==-1){
+        perror("Error while establishing Connection");
+    }
+    
+    printf("Connection with server established");
+
+    char buf[100];
+    //read fron server
+    read(socktd,buf,100);
+    printf("Data from server: %s\n",buf);
+
+    printf("Write massage for server: ");
+    scanf("%[^\n]",buf);
+
+    write(socktd,buf,sizeof(buf));
+    printf("Data sent to server");
+
+    //closing socket
+    close(socktd);
+}
+
+/*
+============================================================================
+Command line: cc 33_client -o client && ./client 
+Output: 
+Socket created
+Connection with server establishedData from server: hello guys whats up!
+Write massage for server: all good here!
+Data sent to server
+============================================================================
+*/
